c8108b0623985ca09477135f6c5948c2955cc76e
Yeshambel <kdusbazezewmekonnen@gmail.com>
2026-01-26 10:18:12 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const swapBtn = document.getElementById("swap-btn");
+
+// Populate dropdowns
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => updateFlag(evt.target));
+}
+
+// Update flag
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+// Fetch live exchange rate
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value || 1;
+  if (amtVal < 1) amtVal = 1;
+
+  try {
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    const rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    const finalAmount = (amtVal * rate).toFixed(2);
+
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Error fetching exchange rate!";
+    console.error("Error:", error);
+  }
+};
+
+// Swap currencies
+swapBtn.addEventListener("click", () => {
+  const temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+  updateExchangeRate();
+});
+
+// Events
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => updateExchangeRate());
---
+++ codes.js
@@ -0,0 +1,158 @@
+// codes.js or app.js
+const countryList = {
+    AED: "AE", // United Arab Emirates Dirham
+    AFN: "AF", // Afghan Afghani
+    ALL: "AL", // Albanian Lek
+    AMD: "AM", // Armenian Dram
+    ANG: "AN", // Netherlands Antillean Guilder
+    AOA: "AO", // Angolan Kwanza
+    ARS: "AR", // Argentine Peso
+    AUD: "AU", // Australian Dollar
+    AWG: "AW", // Aruban Florin
+    AZN: "AZ", // Azerbaijani Manat
+    BAM: "BA", // Bosnian and Herzegovina Convertible Mark
+    BBD: "BB", // Barbadian Dollar
+    BDT: "BD", // Bangladeshi Taka
+    BGN: "BG", // Bulgarian Lev
+    BHD: "BH", // Bahraini Dinar
+    BIF: "BI", // Burundian Franc
+    BMD: "BM", // Bermudan Dollar
+    BND: "BN", // Brunei Dollar
+    BOB: "BO", // Bolivian Boliviano
+    BRL: "BR", // Brazilian Real
+    BSD: "BS", // Bahamian Dollar
+    BTN: "BT", // Bhutanese Ngultrum
+    BWP: "BW", // Botswanan Pula
+    BYN: "BY", // Belarusian Ruble
+    BZD: "BZ", // Belize Dollar
+    CAD: "CA", // Canadian Dollar
+    CDF: "CD", // Congolese Franc
+    CHF: "CH", // Swiss Franc
+    CLP: "CL", // Chilean Peso
+    CNY: "CN", // Chinese Yuan
+    COP: "CO", // Colombian Peso
+    CRC: "CR", // Costa Rican Colón
+    CUP: "CU", // Cuban Peso
+    CVE: "CV", // Cape Verdean Escudo
+    CZK: "CZ", // Czech Republic Koruna
+    DJF: "DJ", // Djiboutian Franc
+    DKK: "DK", // Danish Krone
+    DOP: "DO", // Dominican Peso
+    DZD: "DZ", // Algerian Dinar
+    EGP: "EG", // Egyptian Pound
+    ERN: "ER", // Eritrean Nakfa
+    ETB: "ET", // Ethiopian Birr
+    EUR: "FR", // Euro (Using France flag as a representative)
+    FJD: "FJ", // Fijian Dollar
+    GBP: "GB", // British Pound Sterling
+    GEL: "GE", // Georgian Lari
+    GHS: "GH", // Ghanaian Cedi
+    GIP: "GI", // Gibraltar Pound
+    GMD: "GM", // Gambian Dalasi
+    GNF: "GN", // Guinean Franc
+    GTQ: "GT", // Guatemalan Quetzal
+    GYD: "GY", // Guyanaese Dollar
+    HKD: "HK", // Hong Kong Dollar
+    HNL: "HN", // Honduran Lempira
+    HRK: "HR", // Croatian Kuna
+    HTG: "HT", // Haitian Gourde
+    HUF: "HU", // Hungarian Forint
+    IDR: "ID", // Indonesian Rupiah
+    ILS: "IL", // Israeli New Sheqel
+    INR: "IN", // Indian Rupee
+    IQD: "IQ", // Iraqi Dinar
+    IRR: "IR", // Iranian Rial
+    ISK: "IS", // Icelandic Króna
+    JMD: "JM", // Jamaican Dollar
+    JOD: "JO", // Jordanian Dinar
+    JPY: "JP", // Japanese Yen
+    KES: "KE", // Kenyan Shilling
+    KGS: "KG", // Kyrgystani Som
+    KHR: "KH", // Cambodian Riel
+    KMF: "KM", // Comorian Franc
+    KRW: "KR", // South Korean Won
+    KWD: "KW", // Kuwaiti Dinar
+    KYD: "KY", // Cayman Islands Dollar
+    KZT: "KZ", // Kazakhstani Tenge
+    LAK: "LA", // Laotian Kip
+    LBP: "LB", // Lebanese Pound
+    LKR: "LK", // Sri Lankan Rupee
+    LRD: "LR", // Liberian Dollar
+    LSL: "LS", // Lesotho Loti
+    LYD: "LY", // Libyan Dinar
+    MAD: "MA", // Moroccan Dirham
+    MDL: "MD", // Moldovan Leu
+    MGA: "MG", // Malagasy Ariary
+    MKD: "MK", // Macedonian Denar
+    MMK: "MM", // Myanma Kyat
+    MNT: "MN", // Mongolian Tugrik
+    MOP: "MO", // Macanese Pataca
+    MRO: "MR", // Mauritanian Ouguiya (old)
+    MRU: "MR", // Mauritanian Ouguiya (new)
+    MUR: "MU", // Mauritian Rupee
+    MVR: "MV", // Maldivian Rufiyaa
+    MWK: "MW", // Malawian Kwacha
+    MXN: "MX", // Mexican Peso
+    MYR: "MY", // Malaysian Ringgit
+    MZN: "MZ", // Mozambican Metical
+    NAD: "NA", // Namibian Dollar
+    NGN: "NG", // Nigerian Naira
+    NIO: "NI", // Nicaraguan Córdoba
+    NOK: "NO", // Norwegian Krone
+    NPR: "NP", // Nepalese Rupee
+    NZD: "NZ", // New Zealand Dollar
+    OMR: "OM", // Omani Rial
+    PAB: "PA", // Panamanian Balboa
+    PEN: "PE", // Peruvian Nuevo Sol
+    PGK: "PG", // Papua New Guinean Kina
+    PHP: "PH", // Philippine Peso
+    PKR: "PK", // Pakistani Rupee
+    PLN: "PL", // Polish Zloty
+    PYG: "PY", // Paraguayan Guarani
+    QAR: "QA", // Qatari Rial
+    RON: "RO", // Romanian Leu
+    RSD: "RS", // Serbian Dinar
+    RUB: "RU", // Russian Ruble
+    RWF: "RW", // Rwandan Franc
+    SAR: "SA", // Saudi Riyal
+    SBD: "SB", // Solomon Islands Dollar
+    SCR: "SC", // Seychellois Rupee
+    SDG: "SD", // Sudanese Pound
+    SEK: "SE", // Swedish Krona
+    SGD: "SG", // Singapore Dollar
+    SLL: "SL", // Sierra Leonean Leone
+    SOS: "SO", // Somali Shilling
+    SRD: "SR", // Surinamese Dollar
+    STD: "ST", // São Tomé and Príncipe Dobra (old)
+    STN: "ST", // São Tomé and Príncipe Dobra (new)
+    SVC: "SV", // Salvadoran Colón
+    SYP: "SY", // Syrian Pound
+    SZL: "SZ", // Swazi Lilangeni
+    THB: "TH", // Thai Baht
+    TJS: "TJ", // Tajikistani Somoni
+    TMT: "TM", // Turkmenistani Manat
+    TND: "TN", // Tunisian Dinar
+    TOP: "TO", // Tongan Pa'anga
+    TRY: "TR", // Turkish Lira
+    TTD: "TT", // Trinidad and Tobago Dollar
+    TWD: "TW", // New Taiwan Dollar
+    TZS: "TZ", // Tanzanian Shilling
+    UAH: "UA", // Ukrainian Hryvnia
+    UGX: "UG", // Ugandan Shilling
+    USD: "US", // United States Dollar
+    UYU: "UY", // Uruguayan Peso
+    UZS: "UZ", // Uzbekistan Som
+    VEF: "VE", // Venezuelan Bolívar Fuerte (old)
+    VES: "VE", // Venezuelan Bolívar Soberano (new)
+    VND: "VN", // Vietnamese Đồng
+    VUV: "VU", // Vanuatu Vatu
+    WST: "WS", // Samoan Tala
+    XAF: "CF", // CFA Franc BEAC (Using Central African Republic flag)
+    XCD: "VC", // East Caribbean Dollar (Using St. Vincent flag)
+    XOF: "SN", // CFA Franc BCEAO (Using Senegal flag)
+    XPF: "PF", // CFP Franc (Using French Polynesia flag)
+    YER: "YE", // Yemeni Rial
+    ZAR: "ZA", // South African Rand
+    ZMW: "ZM", // Zambian Kwacha
+    ZWL: "ZW", // Zimbabwean Dollar
+};
